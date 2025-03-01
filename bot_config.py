a98f0acb724f34b677fd2d3ea424d606767cdda1
Artemka <msiboost@mail.ru>
2025-03-02 01:19:13 +0400

    Initial commit

--- bot_config.py
+++ bot_config.py
@@ -1,3 +1,3 @@
 # Конфигурация бота
 BOT_TOKEN = "7631766370:AAF4mf7nrDIKBDCwNpRMupADnXvoyixP0R4"
-WEBAPP_URL = "https://your-username.github.io/petverse"  # Замените на ваш URL 
+WEBAPP_URL = "https://sevenartq.github.io/petverse" 
