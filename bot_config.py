627fd4cbc08d7fc215775541cb2d4b48b3d74e31
Artemka <msiboost@mail.ru>
2025-03-02 01:39:32 +0400

    Add main interface

--- bot_config.py
+++ bot_config.py
@@ -1,3 +1,3 @@
 # Конфигурация бота
 BOT_TOKEN = "7631766370:AAF4mf7nrDIKBDCwNpRMupADnXvoyixP0R4"
-WEBAPP_URL = "https://sevenartq.github.io/petverse"  # Вернем к базовому URL 
+WEBAPP_URL = "https://stunning-froyo-aa65b7.netlify.app"  # Используйте URL, который предоставил Netlify 
